7244a63aa87db2b9a91288b96477028a68d52a60
DMM-IT <macaraigdrew99@gmail.com>
2026-08-15 15:42:06 +0800

    chore: remove hardcoded mailgun secrets from test scripts

--- test-emails-all.php
+++ test-emails-all.php
@@ -4,14 +4,7 @@ $app = require_once __DIR__ . '/bootstrap/app.php';
 $kernel = $app->make(Illuminate\Contracts\Console\Kernel::class);
 $kernel->bootstrap();
 
-config([
-    'mail.default' => 'mailgun',
-    'mail.from.address' => 'noreply@amigagracia.com',
-    'mail.from.name' => 'Amiga Gracia Travel Service',
-    'services.mailgun.domain' => 'mg.amigagracia.com',
-    'services.mailgun.secret' => 'dd0d6edad2a1963baac850c999fc6f91-11c539c0-10a2c92c',
-    'services.mailgun.endpoint' => 'api.mailgun.net',
-]);
+
 
 $email = 'drewmacaraig84@gmail.com';
 
