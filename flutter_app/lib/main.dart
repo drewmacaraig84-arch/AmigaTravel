83bc8a8af9c475d426f261e31356e6e2ca02e5a1
arskngn <arieskingnieto@gmail.com>
2026-08-18 02:14:48 +0800

    bump flutter app version to 1.0.99+110 and update APK

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -97,7 +97,7 @@ class UserSession {
   static String? autoApplyVoucherCode;
 
   // Match this with pubspec.yaml version
-  static const String appVersion = '1.0.98+109';
+  static const String appVersion = '1.0.99+110';
   static String installedAppVersion = appVersion;
 
   static Future<void> init() async {
