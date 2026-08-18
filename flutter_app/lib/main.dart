95ccafac6a5f0eeb31c2edcde2a08d338925fdbd
arskngn <arieskingnieto@gmail.com>
2026-08-18 21:08:37 +0800

    Bump version to 1.0.108+119 and update release AAB and APK in public/downloads

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -99,7 +99,7 @@ class UserSession {
   static String? autoApplyVoucherCode;
 
   // Match this with pubspec.yaml version
-  static const String appVersion = '1.0.107+118';
+  static const String appVersion = '1.0.108+119';
   static String installedAppVersion = appVersion;
 
   static Future<void> init() async {
