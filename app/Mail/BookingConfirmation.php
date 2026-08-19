baada5df38ad67df836fa46537f1f5e260d5804b
DMM-IT <macaraigdrew99@gmail.com>
2026-08-19 20:37:16 +0800

    Fix mailable attachment resolution by sending booking confirmation synchronously

--- app/Mail/BookingConfirmation.php
+++ app/Mail/BookingConfirmation.php
@@ -5,11 +5,10 @@ namespace App\Mail;
 use App\Models\Booking;
 use Illuminate\Bus\Queueable;
 use Illuminate\Mail\Mailable;
-use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Facades\Storage;
 
-class BookingConfirmation extends Mailable implements ShouldQueue
+class BookingConfirmation extends Mailable
 {
     use Queueable, SerializesModels;
 
