ad5759711bc9ffa189ae2f3e69e93c20e7435aed
DMM-IT <macaraigdrew99@gmail.com>
2026-08-21 23:29:15 +0800

    build: bump mobile version to 1.0.123+134 and update release APK/AAB in public downloads

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -100,7 +100,7 @@ class UserSession {
   static String? autoApplyVoucherCode;
 
   // Match this with pubspec.yaml version
-  static const String appVersion = '1.0.121+132';
+  static const String appVersion = '1.0.123+134';
   static String installedAppVersion = appVersion;
 
   static Future<void> init() async {
