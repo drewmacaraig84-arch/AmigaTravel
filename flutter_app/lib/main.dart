b63f2222bad1bda83ba3946aa4333e3ca1d5dd0e
DMM-IT <macaraigdrew99@gmail.com>
2026-08-29 16:34:31 +0800

    Sync appVersion in main.dart to 1.0.138+149

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -100,7 +100,7 @@ class UserSession {
   static String? autoApplyVoucherCode;
 
   // Match this with pubspec.yaml version
-  static const String appVersion = '1.0.137+148';
+  static const String appVersion = '1.0.138+149';
   static String installedAppVersion = appVersion;
 
   static Future<void> init() async {
