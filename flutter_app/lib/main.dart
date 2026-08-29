c3cd1e2e3813bec0a22d9f512ff5f78566c4c4a9
DMM-IT <macaraigdrew99@gmail.com>
2026-08-29 10:05:50 +0800

    chore: bump app version to 1.0.137+148, build release APK and AAB bundle, and update download page

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -100,7 +100,7 @@ class UserSession {
   static String? autoApplyVoucherCode;
 
   // Match this with pubspec.yaml version
-  static const String appVersion = '1.0.136+147';
+  static const String appVersion = '1.0.137+148';
   static String installedAppVersion = appVersion;
 
   static Future<void> init() async {
